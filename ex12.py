8e771d80e61aad0e511ecb897d43d0a0e0d714d4
Justin Dorfman <justindorfman@gmail.com>
2012-01-07 15:51:07 -0800

    Excercise 12

---
+++ ex12.py
@@ -0,0 +1,32 @@
+# Excercise 12 - http://learnpythonthehardway.org/book/ex12.html
+
+age = raw_input("How old are you? " )
+height = raw_input("How tall are you? " )    
+weight = raw_input("How much do you weigh? " )
+
+print "So, you are %r years old, %r tall and %r heavy" % (age, height, weight) # sorry Zed, I want to keep years old.
+
+# Extra Credit
+# 1: Instead of python ex12.py run pydoc raw_input, I am guessing it will print out a man page for raw_input?  Sweet that will come in handy.
+# 2: I hit q to quit before I even read what assignment 2 was.  Wow I am a genius! 
+# 3: Google-ing pydoc brb k?  I'm back, since I swore if Copy and Paste I will type it out:
+# The pydoc module automatically generates documentation from Python modules.  The documentation can be presented on the console, served to a web browser or saved to html files.
+# 4: pydoc the following modules: open, file, os & sys. Again no copy and paste.
+
+# pydoc open: 
+# Open a file using the file() type, returns a file object. WTF? that is poor documentation imo.
+
+# That sucks gedit crashed for the second time today so now I have to redo 'file'...
+
+# pydoc file:
+# Open a file. The mode can be'r', 'w' or 'a' (read, write, append).  If the file doesn't exist it will be created for writing or appending.
+
+# brb got to walk the dog.
+
+# pydoc os:
+# OS routines for Mac, NT, or Posix depending on what system the python program/script is on.
+
+# pydoc sys:
+# This module provides access to some objects used or maintained by the interpreter and to functions that interact strongly with the interpreter.
+
+# Third time gedit crashed... Zed I might need to jump ship to smultron or textmate 2 (it was trending on twitter a few months back so it must be good...)
