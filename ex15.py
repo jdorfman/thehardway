b75f20b6e42b9bb97a3846f6ae359e37ef23530e
Justin Dorfman <justindorfman@gmail.com>
2012-01-07 23:11:47 -0800

    Extra Credit #5

--- ex15.py
+++ ex15.py
@@ -1,31 +1,31 @@
 # Excercise 15 - http://learnpythonthehardway.org/book/ex15.html
 
-from sys import argv
+##from sys import argv
 
 # filename = ex15_sample.txt
-script, filename = argv
+##script, filename = argv
 
 # txt = open(ex15_sample.txt)
-txt = open(filename)
+##txt = open(filename)
 
 # print: Here is your file 'ex15_sample.txt'
-print "Here is your file %r" % filename
+##print "Here is your file %r" % filename
 # open ex15_sample.txt and read it (print it)
-print txt.read()
+##print txt.read()
 
 # ask the user to type the file name
-##print "Type that filename again:"
-##file_again = raw_input("> ")
+print "Type that filename again:"
+file_again = raw_input("> ")
 
 # create a variable that will open ex15_sample.txt
-##txt_again = open(file_again)
+txt_again = open(file_again)
 
 # print the variable and read it (print out ex15_sample.txt)
-##print txt_again.read()
+print txt_again.read()
 
 # Extra Credit
 # 1: Already started to write out what each line does in english.  Excercise by excercise I am starting to think like Zed...
 # 2: If you are ever stumped google 'python THING' e.g. "python open"
 # 3: comands are also called functions and methods
-# 4: commenting out lines 17-24 and running the script again
-# 5: commenting out lines 3 - 14 and runing the script again
+# 4: commenting out lines 17-24 and running the script again - https://github.com/jdorfman/thehardway/commit/86dbc37e8da3f2ff31127826b3205a244600e98b
+# 5: commenting out lines 3 - 14 and runing the script again - 
