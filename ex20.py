ff3d2b8a09ed8ec7c76e6e55676838e6d88b923c
Justin Dorfman <justindorfman@gmail.com>
2012-01-13 19:05:02 -0800

    ex20 finish tomorrow

---
+++ ex20.py
@@ -0,0 +1,13 @@
+# Excercise 20 - http://learnpythonthehardway.org/book/ex20.html
+
+from sys import argv
+
+script, input_file = argv
+
+def print_all(f(:
+    print f.read()
+    
+def rewind(f)
+    print f.seek(0)
+    
+
