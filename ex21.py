9d4cbbacecd9b988d8e4049fca0a817f09d4c557
Justin Dorfman <justindorfman@gmail.com>
2012-01-15 12:32:44 -0800

    Still Cleaning up, looking much better imo

--- ex21.py
+++ ex21.py
@@ -1,5 +1,7 @@
 # Excercise 21 - http://learnpythonthehardway.org/book/ex21.html
 
+nl = "\n" # New Line
+
 def add(a, b):
     print "ADDING %d + %d" % (a, b)
     return a + b
@@ -16,6 +18,7 @@ def divide(a, b):
     print "DIVIDING %d / %d" % (a, b)
     return a / b
 
+print nl # New Line
 print "Let's do some math with just fucntions!"
 
 age = add (30, 5)
@@ -27,6 +30,7 @@ print "Age: %d Height: %d Weight: %d IQ: %d" % (age, height, weight, iq)
 
 # A puzzle for Extra Credit
 
+print nl
 print "Here is a puzzle:"
 
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
@@ -38,6 +42,7 @@ print "That becomes", what, "Can you do it by hand?"
 # You can return anything that you can put to the right of an =
 
 # EC 1a.
+print nl
 print "\n Now some Extra Credit!\n"
 def rt(a, b): #rt = returntest
     print "%d \n+%d" % (a, b)
@@ -47,15 +52,15 @@ def rt(a, b): #rt = returntest
 add = rt(30, 4)
 
 print add
+print nl
 
 # EC 1b.
 def rt2(a, b, c, d):
     print "%d + %d * %d - %d" % (a, b, c, d)
     return a + b * c - d
 
-num = rt2(30, 58, 95, 10)
-
-print num
+num = rt2(30, 58, 95, 10) 
+print "=", num, nl
 
 #EC 1c.
 def hello(a, b, c, d):
@@ -68,26 +73,26 @@ w4 = 'yeah.'
 
 world = hello(w1, w2, w3, w4)
 
-print world
+print world, nl
 
 # EC 1d.
 s = ' ' #space
 def words(a, b, c, d):
-    #return a + b + c + d
-    return d + s + a + s + b + s + c # Changing it up
+    return a + s + b + s + c + s + d
+    #return d + s + a + s + b + s + c # Changing it up
 
 print "Pick four words:"
 print "Word 1:"
-w1 = raw_input()
+w1 = raw_input('> ')
 print "Word 2:"
-w2 = raw_input()
+w2 = raw_input('> ')
 print "Word 3:"
-w3 = raw_input()
+w3 = raw_input('> ')
 print "Word 4:"
-w4 = raw_input()
+w4 = raw_input('> ')
 
 final = words(w1, w2, w3, w4)
-print final
+print nl, final, nl
 
 # 2: What you should do is try to figure out the normal formula that would recreate this same set of operations.
 # what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
@@ -100,15 +105,19 @@ print final
 
 
 def DIV(a, b):
+    print "Dividing %d / %d" % (a, b)
     return a / b
 
 def MUL(a, b):
+    print "Multiplying %d * %d" % (a, b)
     return a * b
 
 def SUB(a, b):
+    print "Subtracting %d - %d" % (a, b)
     return a - b
 
 def ADD(a, b):
+    print "Adding %d + %d" % (a, b)
     return a + b
 
 ec2a = DIV(50, 2)
@@ -117,6 +126,7 @@ ec2c = SUB(74, ec2b)
 ec2d = ADD(35, ec2c)
 
 print "That becomes %d Can you do it by hand?" % (ec2d)
+print nl
 
 # 3: Once you have the formula worked out for the puzzle, get in there and see what happens when you modify the parts of the functions. 
 # Try to change it on purpose to make another value.
@@ -142,7 +152,7 @@ ec2b = MUL(180, ec2a)
 ec2c = SUB(74, ec2b)
 ec2d = ADD(35, ec2c)
 
-print "Div: %d Mul: %d Sub: %d Add: %d" % (ec2a, ec2b, ec2c, ec2d)
+print nl, "Div: %d Mul: %d Sub: %d Add: %d" % (ec2a, ec2b, ec2c, ec2d)
 
 print "That becomes %d Can you do it by hand?" % (ec2d)
 
