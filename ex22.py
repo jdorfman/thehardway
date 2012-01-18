b661b83d4b23d0cd12609b56d5adb5a3b0e150b4
Justin Dorfman <justindorfman@gmail.com>
2012-01-17 22:35:24 -0800

    don't run this at home

---
+++ ex22.py
@@ -0,0 +1,79 @@
+# Excercise 22 - http://learnpythonthehardway.org/book/ex22.html
+
+# I know Zed said to write it down, which I will, I still want a digital copy.
+
+# First, go back through every exercise you have done so far and write down every word and symbol (another name for 'character') that you have used. 
+# Make sure your list of symbols is complete.
+
+# Excercise 1
+
+# = Comment. Also called: Pound, hash, octothrope, mesh. They can go above your code or after you code like this: print "Test" #printing test
+^ = Carrot
+SyntaxError = Tells us what that error might be. Usually they are cryptic, but if you google them you will find the answer most of the time.
+print = displays the text you define on the screen
+
+Math Symbols:
++ = Plus (for adding)
+- = Minus (for subtracting)
+/ = slash (for dividing)
+* = asterisk aka star (for multiplying)
+% = percent
+< = less-than
+> = grater-than
+<= = less-than-equal
+>= = greater-than-equal
+
+floating-point numbera = numbers with a decimal e.g.: 1000.0 is a floating point number
+
+variable = a symbolic name given to some known or unknown quantity or value. Programmers use variables to make their code read more like english.
+
+_ = underscore character. We use this character to put an imaginary space between words in variable names.
+
+string = Everytime you put "" aka double quotes around a piece of text you make a string. You print them, save them to files, send them to servers, etc.
+
+multi-line string = """
+this is an example of a multi-line string
+"""
+
+Format strings:
+%r = print this no matter what
+%s = Allows me to insert (and potentially format) a string
+%d = serves as a placeholder for a signed integer decimal value.   
+
+\n = new line
+\t = tab
+
+"I am 6'2\" tall."  # escape double-quote inside string
+'I am 6\'2" tall.'  # escape single-quote inside string
+
+raw_input() = allows the user to input characters.
+
+pydoc = allows you to view help documents about any python function. e.g. pydoc raw_input
+
+open = Open a file using the file() type, returns a file object.
+file = Open a file.  The mode can be 'r', 'w' or 'a' for reading (default), writing or appending. 
+
+Modules (Libraries) - e.g.: from sys import argv
+os = operating system routines for Mac, NT, or Posix depending on what system we're on.
+os.path = Common operations on Posix pathnames.
+sys = This module provides access to some objects used or maintained by the interpreter and to functions that interact strongly with the interpreter.
+ * argv = command line arguments e.g. $ python text.py argument
+
+close -- Closes the file. Like File->Save.. in your editor.
+read -- Reads the contents of the file, you can assign the result to a variable.
+readline -- Reads just one line of a text file.
+truncate -- Empties the file, watch out if you care about the file.
+write(stuff) -- Writes stuff to the file.
+
+def = Function definition. Allows you to create your own functions (mini scripts, tiny commands)
+
+To 'run', 'call', or 'use' a function all mean the same thing.
+
+'+=' = adds another value with the variable's value and assigns the new value to the variable. (Shorthand notation)
+
+return = to set variables to be a value from a function 
+
+
+
+ 
+
