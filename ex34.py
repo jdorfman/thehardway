f3cfb0a6d8c4ac60a2971e35bb1ae9941c85e661
Justin Dorfman <justindorfman@gmail.com>
2012-04-01 16:50:00 -0700

    Ex34

---
+++ ex34.py
@@ -0,0 +1,51 @@
+# http://learnpythonthehardway.org/book/ex34.html
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+one = animals[1]
+print "The animal at 1 is %s" % one
+
+two = animals[3]
+print "The 3rd animal is %s" % two
+
+three = animals[0]
+print "The 1st animal is %s" % three
+
+four = animals[3]
+print "The animal at 3 is %s" % four
+
+five = animals[4]
+print "The 5th animal is a %s" % five
+
+six = animals[2]
+print "The animal at 2 is %s" % six
+
+seven = animals[5]
+print "The sixth animal is a %s" % seven
+
+eight = animals[4]
+print "The animal at four is a %s" % eight
+
+# Extra Credit 
+# 1. Read about ordinal and cardinal numbers online.
+# 2. With what you know of the difference between these types of numbers, can you explain why the year 2010 in "January 1, 2010" really is 2010 and not 2009? (Hint, you can't pick years at random.)
+# 3. Write some more lists and work out similar indexes until you can translate them.
+# 4. Use Python to check your answers to this as well.
+
+# Zoe's Nick Names
+znn = ['Punka', 'Princess', 'Punkers', 'Punky', 'Grumpers']
+
+two = znn[2]
+four = znn[4]
+print "When Zoe whines we call her %s %s." % (four, two)
+
+zero = znn[0]
+print "Lately I have been calling her %s Jam." % zero
+
+one = znn[1]
+print "Stevie loves calling Zoe %s." % one
+
+print "She usually calls her a Pretty Pretty %s." % one
+
+
+
+
